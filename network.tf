7680a9d7276014f5c3ab53be12b61236f104afe6
Dan Tracey <dtracey@hotmail.co.uk>
2023-03-14 20:32:26 +0000

    First Commit

---
+++ network.tf
@@ -0,0 +1,47 @@
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = ["10.0.1.0/22"]
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg1.location
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+}
+
+
+
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "vnet2"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = ["10.0.2.0/22"]
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+}
+
+resource "azurerm_virtual_network_peering" "peer1to2" {
+  name                      = "peer1to2"
+  resource_group_name       = azurerm_resource_group.rg1.name
+  virtual_network_name      = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet2.id
+}
+
+resource "azurerm_virtual_network_peering" "peer2to1" {
+  name                      = "peer2to1"
+  resource_group_name       = azurerm_resource_group.rg1.name
+  virtual_network_name      = azurerm_virtual_network.vnet2.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
+}
