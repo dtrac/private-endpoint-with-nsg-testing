1ef5e9d50ab5f16471d2f9f373fe81cebf0f0f04
dtrac <dtracey@hotmail.co.uk>
2023-03-14 20:39:28 +0000

    tf fmt

--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
-  address_space       = ["10.0.1.0/22"]
+  address_space       = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "subnet1" {
@@ -24,7 +24,7 @@ resource "azurerm_virtual_network" "vnet2" {
   name                = "vnet2"
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
-  address_space       = ["10.0.2.0/22"]
+  address_space       = ["10.0.2.0/24"]
 
   subnet {
     name           = "subnet2"
